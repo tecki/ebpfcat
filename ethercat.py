eb2288d2bdcae07f5ee6cd2d3df5870ae7bab9db
Martin Teichmann <martin.teichmann@xfel.eu>
2020-12-11 16:41:08 +0000

    read the object description list

--- ethercat.py
+++ ethercat.py
@@ -22,14 +22,76 @@ class ECCmd(Enum):
    ARMW = 13  # Auto Increment Read Multiple Write
    FRMW = 14  # Configured Read Multiple Write
 
+
+class ECDatatype(Enum):
+   BOOLEAN = 0x1
+   INTEGER8 = 0x2
+   INTEGER16 = 0x3
+   INTEGER32 = 0x4
+   UNSIGNED8 = 0x5
+   UNSIGNED16 = 0x6
+   UNSIGNED32 = 0x7
+   REAL32 = 0x8
+   VISIBLE_STRING = 0x9
+   OCTET_STRING = 0xA
+   UNICODE_STRING = 0xB
+   TIME_OF_DAY = 0xC
+   TIME_DIFFERENCE = 0xD
+   DOMAIN = 0xF
+   INTEGER24 = 0x10
+   REAL64 = 0x11
+   INTEGER64 = 0x15
+   UNSIGNED24 = 0x16
+   UNSIGNED64 = 0x1B
+   BIT1 = 0x30
+   BIT2 = 0x31
+   BIT3 = 0x32
+   BIT4 = 0x33
+   BIT5 = 0x34
+   BIT6 = 0x35
+   BIT7 = 0x36
+   BIT8 = 0x37
+
+class MBXType(Enum):
+   ERR = 0  # Error
+   AOE = 1  # ADS over EtherCAT
+   EOE = 2  # Ethernet over EtherCAT
+   COE = 3  # CANopen over EtherCAT
+   FOE = 4  # File over EtherCAT
+   SOE = 5  # Servo over EtherCAT
+   VOE = 0xf  # Vendor over EtherCAT
+
+class CoECmd(Enum):
+   EMERGENCY = 1
+   SDOREQ = 2
+   SDORES = 3
+   TXPDO = 4
+   RXPDO = 5
+   TXPDO_RR = 6
+   RXPDO_RR = 7
+   SDOINFO = 8
+
+class SDOCmd(Enum):
+   DOWN_INIT = 0x21
+   DOWN_EXP = 0x23
+   DOWN_INIT_CA = 0x31
+   UP_REQ = 0x40
+   UP_REQ_CA = 0x50
+   SEG_UP_REQ = 0x60
+   ABORT = 0x80
+
 class ODCmd(Enum):
-   ODLIST_REQ = 1
-   ODLIST_RES = 2
+   LIST_REQ = 1
+   LIST_RES = 2
    OD_REQ = 3
    OD_RES = 4
    OE_REQ = 5
    OE_RES = 6
    SDOINFO_ERROR = 7
+
+
+class ObjectDescription:
+    pass
 
 
 class AsyncBase:
@@ -108,9 +170,25 @@ class Terminal:
     async def initialize(self, relative, absolute):
         await self.ec.roundtrip(ECCmd.APWR, relative, 0x10, "H", absolute)
         self.position = absolute
+
+
+        async def read_eeprom(no, fmt):
+            return unpack(fmt, await self.eeprom_read_one(no))
+
+        self.vendorId, self.productCode = await read_eeprom(8, "<II")
+        self.revisionNo, self.serialNo = await read_eeprom(0xc, "<II")
+        # this reads the mailbox configuration from the EEPROM header.
+        # weirdly this does not match with the later EEPROM SM configuration
+        # self.mbx_in_off, self.mbx_in_sz, self.mbx_out_off, self.mbx_out_sz = \
+        #     await read_eeprom(0x18, "<HHHH")
+
+        self.mbx_cnt = 1
+
         self.eeprom = await self.read_eeprom()
         await self.write(0x800, 0x80)  # empty out sync manager
         await self.write(0x800, self.eeprom[41])
+        self.mbx_out_off, self.mbx_out_sz, self.mbx_in_off, self.mbx_in_sz = \
+            unpack("<HH4xHH", self.eeprom[41][:12])
 
     async def set_state(self, state):
         await self.ec.roundtrip(ECCmd.FPWR, self.position, 0x0120, "H", state)
@@ -182,6 +260,72 @@ class Terminal:
                 return eeprom
             eeprom[hd] = await get_data(ws * 2)
 
+    async def mbx_send(self, type, data, address=0, priority=0, channel=0):
+        status, = await self.read(0x805, "B")  # always using mailbox 0, OK?
+        if status & 8:
+            raise RuntimeError("mailbox full, read first")
+        head = pack("<HHBB", len(data), address, channel | priority << 6,
+                    type.value | self.mbx_cnt << 4)
+        await gather(self.write(self.mbx_out_off, head + data),
+                     self.write(self.mbx_out_off + self.mbx_out_sz - 1, 1))
+        self.mbx_cnt = self.mbx_cnt % 7 + 1  # yes, we start at 1 not 0
+
+    async def mbx_recv(self):
+        status = 0
+        while status & 8 == 0:
+            status, = await self.read(0x80D, "B")  # always using mailbox 1, OK?
+        data = await self.read(self.mbx_in_off, self.mbx_in_sz)
+        dlen, address, prio, type = unpack("<HHBB", data[:6])
+        return MBXType(type & 0xf), data[6 : dlen+6]
+
+    async def read_ODlist(self):
+        cmd = pack("<HBxHH", CoECmd.SDOINFO.value << 12,
+                   ODCmd.LIST_REQ.value, 0, 1)
+        await self.mbx_send(MBXType.COE, cmd)
+
+        fragments = True
+        offset = 8  # skip header in first packet
+        indexes = []
+
+        while fragments:
+            type, data = await self.mbx_recv()
+            if type is not MBXType.COE:
+                raise RuntimeError(f"expected CoE package, got {type}")
+            coecmd, odcmd, fragments = unpack("<HBxH", data[:6])
+            coecmd = CoECmd(coecmd >> 12)
+            odcmd = ODCmd(odcmd & 0x7f)
+            if odcmd is not ODCmd.LIST_RES:
+                raise RuntimeError(f"expected LIST_RES, got {odcmd}")
+
+            indexes.extend(unpack("<" + "H" * int((len(data) - offset) // 2),
+                                  data[offset:]))
+            offset = 6
+
+        ret = []
+
+        for index in indexes:
+            cmd = pack("<HBxHH", CoECmd.SDOINFO.value << 12,
+                       ODCmd.OD_REQ.value, 0, index)
+            await self.mbx_send(MBXType.COE, cmd)
+
+            type, data = await self.mbx_recv()
+            if type is not MBXType.COE:
+                raise RuntimeError(f"expected CoE package, got {type}")
+            coecmd, odcmd, fragments, dtype, oc, ms = unpack("<HBxH2xHBB", data[:12])
+            coecmd = CoECmd(coecmd >> 12)
+            odcmd = ODCmd(odcmd & 0x7f)
+            if odcmd is not ODCmd.OD_RES:
+                raise RuntimeError(f"expected OD_RES, got {odcmd}")
+
+            od = ObjectDescription()
+            od.index = index
+            od.dataType = dtype  # ECDataType(dtype)
+            od.maxSub = ms
+            od.name = data[12:].decode("utf8")
+            ret.append(od)
+
+        return ret
+
 
 async def main():
     ec = await EtherCat("eth0")
@@ -197,6 +341,9 @@ async def main():
     await tin.to_operational(),
     print("tout")
     await tout.to_operational(),
+    odlist = await tout.read_ODlist()
+    for o in odlist:
+        print(o.name)
     print("tdigi")
     await tdigi.to_operational(),
 
