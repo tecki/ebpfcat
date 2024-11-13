e4d25898d0c3ed69cb83c71ce735d2fb3f160179
Martin Teichmann <martin.teichmann@xfel.eu>
2024-11-13 10:27:40 +0100

    TMP more lock

--- ebpfcat/lock.py
+++ ebpfcat/lock.py
@@ -4,26 +4,30 @@ from asyncio import Lock, sleep
 
 
 class LockFile:
-    def __init__(self, name, mininum, maximum):
-        self.filename = f'/run/ebpf/{name}'
+    def __init__(self, filename, minimum, maximum):
+        self.filename = filename
         self.minimum = minimum
         self.maximum = maximum
+        os.makedirs(filename.rsplit('/', 1)[0], exist_ok=True)
         try:
             self.file = open(self.filename, 'xb')
-        except FileNotFoundError:
+        except FileExistsError:
             self.file = open(self.filename, 'r+b')
         else:
             self.file.write(bytes(maximum - minimum))
+        self.fd = self.file.fileno()
 
     def close(self):
         self.file.close()
 
     def remove(self):
-        os.
+        os.remove(self.filename)
 
-    def get_lock(self, no):
-        assert self.minimum <= no < self.maximum
-        return MailboxLock(self.file.fileno(), no - self.minimum)
+    def __getstate__(self):
+        return self.filename, self.minimum, self.maximum
+
+    def __setstate__(self, state):
+        self.__init__(*state)
 
 
 class MailboxLock(Lock):
@@ -39,24 +43,26 @@ class MailboxLock(Lock):
 
 
 class ParallelMailboxLock:
-    def __init__(self, fd, no):
-        self.fd = fd
-        self.no = no
+    def __init__(self, lock_file, no):
+        self.lock_file = lock_file
+        assert self.lock_file.minimum <= no < self.lock_file.maximum
+        self.no = no - self.lock_file.minimum
         self.counter = None
 
     async def __aenter__(self):
         while True:
             try:
-                fcntl.lockf(self.fd, fcntl.LOCK_NB | fcntl.LOCK_EX, 1, self.no)
+                fcntl.lockf(self.lock_file.fd, fcntl.LOCK_NB | fcntl.LOCK_EX,
+                            1, self.no)
             except OSError:
                 await sleep(0)
                 continue
             break
-        self.counter, = os.pread(self.fd, 1, self.no)
+        self.counter, = os.pread(self.lock_file.fd, 1, self.no)
 
     async def __aexit__(self, a, b, c):
-        os.pwrite(self.fd, bytes((self.counter,)), self.no)
-        fcntl.lockf(self.fd, fcntl.LOCK_UN, 1, self.no)
+        os.pwrite(self.lock_file.fd, bytes((self.counter,)), self.no)
+        fcntl.lockf(self.lock_file.fd, fcntl.LOCK_UN, 1, self.no)
         self.counter = None
 
     def next_counter(self):
