e540c6e37df88278860b422f105d54826a7879a8
Martin Teichmann <martin.teichmann@xfel.eu>
2026-01-15 12:03:30 +0100

    fix typo

--- ebpfcat/ethercat.py
+++ ebpfcat/ethercat.py
@@ -574,7 +574,7 @@ class Terminal:
     name = 'No Name'
 
     def __repr__(self):
-        return f'{self.__class__.__name__}.("{self.name}")'
+        return f'{self.__class__.__name__}("{self.name}")'
 
     def __str__(self):
         return self.name
